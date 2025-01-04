da086d0dde65e65f4f04019b16b10a83b72a08cc
ornob39 <ornob39@gmail.com>
2025-01-04 14:30:54 +0600

    Remove GPU setup function from main.py

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-import tensorflow as tf
 from utils.preprocessing import prepare_data
 from models.xgboost_model import train_xgboost
 from models.prophet_model import train_prophet
 from models.ensemble import evaluate_predictions
 from catboost import CatBoostRegressor
 
-def setup_gpu():
-    gpus = tf.config.list_physical_devices('GPU')
-    if gpus:
-        try:
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.list_logical_devices('GPU')
-            print(f"{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs")
-        except RuntimeError as e:
-            print(e)
 
 def main():
-    setup_gpu()
+
     df = pd.read_csv('data/continuous dataset.csv')
     
     X_train_scaled, X_test_scaled, y_train, y_test, feature_names, train_df, test_df = prepare_data(df, return_dfs=True)
